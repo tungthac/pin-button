c260fc746b40ca85ea539e8d560ac3985d366894
Nguyen Thac Tung <tung@mohasoftware.com>
2026-05-13 17:02:58 +0700

    OPERATION: `pin.toggle` method exists -> FAIL

--- test/unit/pin.test.ts
+++ test/unit/pin.test.ts
@@ -379,6 +379,38 @@ state(State.STATUS, () => {
   });
 });
 
+// Operation — Toggle composite group
+operation(Operation.TOGGLE, () => {
+  and("Pin is defined in custom element registry", () => {
+    beforeEach(() => {
+      define(Pin.Tag, Pin);
+    });
+
+    and("HTML Template is added to DOM", () => {
+      beforeEach(async () => {
+        await Pin.Template.load("pin.template.html");
+      });
+      afterEach(() => {
+        remove(Pin.Tag);
+      });
+
+      and("a new pin is added to DOM", () => {
+        let pin: Pin;
+        beforeEach(() => {
+          pin = add<Pin>(Pin.Tag);
+        });
+        afterEach(() => {
+          pin.remove();
+        });
+
+        then("`pin.toggle` method exists", () => {
+          expect(pin.toggle).toBeDefined();
+        });
+      });
+    });
+  });
+});
+
 // Operation — Pin/Unpin paired group
 operation(Operation.PIN, () => {
   and("Pin is defined in custom element registry", () => {

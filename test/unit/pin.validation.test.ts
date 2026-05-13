09e0a58a6c6fbf2c022ffd7a0b979747da319c3f
Nguyen Thac Tung <tung@mohasoftware.com>
2026-05-13 16:32:40 +0700

    VALIDATION: `Validate.visibility` static method exists -> FAIL

---
+++ test/unit/pin.validation.test.ts
@@ -0,0 +1,16 @@
+import { State, Validate } from "@tungthac/pin-button";
+
+// Visibility
+validation(State.VISIBILITY, () => {
+  given("Validate imported", () => {
+    then("Validate is defined", () => {
+      expect(Validate).toBeDefined();
+    });
+
+    and("Validate is defined", () => {
+      then("`Validate.visibility` static method is defined", () => {
+        expect(Validate.visibility).toBeDefined();
+      });
+    });
+  });
+});

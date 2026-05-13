893fefa67e562a2584f8d61049f7ee6443ea316b
Nguyen Thac Tung <tung@mohasoftware.com>
2026-05-13 17:05:30 +0700

    EVENT: `pin.onpin` setter exists -> FAIL

--- test/unit/pin.test.ts
+++ test/unit/pin.test.ts
@@ -767,4 +767,35 @@ events(Event.ON_SHOW, () => {
   });
 });
 
+events(Event.ON_PIN, () => {
+  and("Pin is defined in custom element registry", () => {
+    beforeEach(() => {
+      define(Pin.Tag, Pin);
+    });
+
+    and("HTML Template is added to DOM", () => {
+      beforeEach(async () => {
+        await Pin.Template.load("pin.template.html");
+      });
+      afterEach(() => {
+        remove(Pin.Tag);
+      });
+
+      and("a new pin is added to DOM", () => {
+        let pin: Pin;
+        beforeEach(() => {
+          pin = add<Pin>(Pin.Tag);
+        });
+        afterEach(() => {
+          pin.remove();
+        });
+
+        then("`pin.onpin` setter exists", () => {
+          expect(hasSetter(pin, Event.ON_PIN)).toBeTrue();
+        });
+      });
+    });
+  });
+});
+
 // Gesture

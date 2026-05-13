a2fd28f67479140c18120430ae2c18460f73cdf3
Nguyen Thac Tung <tung@mohasoftware.com>
2026-05-13 16:40:44 +0700

    EVENT: `pin.onhide` setter exists -> FAIL

--- test/unit/pin.test.ts
+++ test/unit/pin.test.ts
@@ -7,7 +7,8 @@ import {
   Attributes,
   State,
   Visibility,
-  Operation
+  Operation,
+  Event
 } from "@tungthac/pin-button";
 
 // Configuration
@@ -339,5 +340,35 @@ operation(Operation.HIDE, () => {
 });
 
 // Event
+events(Event.ON_HIDE, () => {
+  and("Pin is defined in custom element registry", () => {
+    beforeEach(() => {
+      define(Pin.Tag, Pin);
+    });
+
+    and("HTML Template is added to DOM", () => {
+      beforeEach(async () => {
+        await Pin.Template.load("pin.template.html");
+      });
+      afterEach(() => {
+        remove(Pin.Tag);
+      });
+
+      and("a new pin is added to DOM", () => {
+        let pin: Pin;
+        beforeEach(() => {
+          pin = add<Pin>(Pin.Tag);
+        });
+        afterEach(() => {
+          pin.remove();
+        });
+
+        then("`pin.onhide` setter exists", () => {
+          expect(hasSetter(pin, Event.ON_HIDE)).toBeTrue();
+        });
+      });
+    });
+  });
+});
 
 // Gesture

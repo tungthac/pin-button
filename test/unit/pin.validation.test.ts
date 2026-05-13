28d9d58b9a31e612e6b0b5686c23f51ce22f4d20
Nguyen Thac Tung <tung@mohasoftware.com>
2026-05-13 16:34:18 +0700

    VALIDATION: `Validate.visibility(value)` throws error if `value` is `invalid` -> FAIL

--- test/unit/pin.validation.test.ts
+++ test/unit/pin.validation.test.ts
@@ -1,4 +1,4 @@
-import { State, Validate } from "@tungthac/pin-button";
+import { State, Visibility, Validate } from "@tungthac/pin-button";
 
 // Visibility
 validation(State.VISIBILITY, () => {
@@ -10,6 +10,93 @@ validation(State.VISIBILITY, () => {
     and("Validate is defined", () => {
       then("`Validate.visibility` static method is defined", () => {
         expect(Validate.visibility).toBeDefined();
+      });
+
+      and("`value` is `Visibility.VISIBLE`", () => {
+        let value: any;
+        beforeEach(() => {
+          value = Visibility.VISIBLE;
+        });
+
+        when("`Validate.visibility(value)` is called", () => {
+          let error: unknown | undefined;
+          let result: any;
+          beforeEach(() => {
+            try {
+              result = Validate.visibility(value);
+            } catch (err) {
+              error = err;
+            }
+          });
+
+          then("no error is thrown", () => {
+            expect(error).toBeUndefined();
+          });
+
+          then("result is `Visibility.VISIBLE`", () => {
+            expect(result).toBe(Visibility.VISIBLE);
+          });
+        });
+      });
+
+      and("`value` is `Visibility.HIDDEN`", () => {
+        let value: any;
+        beforeEach(() => {
+          value = Visibility.HIDDEN;
+        });
+
+        when("`Validate.visibility(value)` is called", () => {
+          let error: unknown | undefined;
+          let result: any;
+          beforeEach(() => {
+            try {
+              result = Validate.visibility(value);
+            } catch (err) {
+              error = err;
+            }
+          });
+
+          then("no error is thrown", () => {
+            expect(error).toBeUndefined();
+          });
+
+          then("result is `Visibility.HIDDEN`", () => {
+            expect(result).toBe(Visibility.HIDDEN);
+          });
+        });
+      });
+
+      and("`value` is `invalid`", () => {
+        let value: any;
+        beforeEach(() => {
+          value = "invalid";
+        });
+
+        when("`Validate.visibility(value)` is called", () => {
+          let error: unknown | undefined;
+          beforeEach(() => {
+            try {
+              Validate.visibility(value);
+            } catch (err) {
+              error = err;
+            }
+          });
+
+          then("an error is thrown", () => {
+            expect(error).not.toBeUndefined();
+          });
+
+          and("an error is thrown", () => {
+            then(
+              "error message contains 'Invalid visibility value: invalid'",
+              () => {
+                expect((error as Error).message).toEqual(
+                  "Invalid visibility value: invalid"
+                );
+              }
+            );
+          });
+        });
       });
     });
   });
